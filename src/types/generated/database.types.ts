28650570803a9bb923cd11e23b8c42b6afa77aa2
Sarim <sarim.xs@gmail.com>
2025-06-23 13:04:00 -0600

    Update types

--- src/types/generated/database.types.ts
+++ src/types/generated/database.types.ts
@@ -360,6 +360,276 @@ export type Database = {
         }
         Relationships: []
       }
+      "ws-chapter-of-the-day": {
+        Row: {
+          chapter_number: number
+          day: number
+          id: string | null
+          month: number
+          year: number
+        }
+        Insert: {
+          chapter_number: number
+          day: number
+          id?: string | null
+          month: number
+          year: number
+        }
+        Update: {
+          chapter_number?: number
+          day?: number
+          id?: string | null
+          month?: number
+          year?: number
+        }
+        Relationships: []
+      }
+      "ws-quran": {
+        Row: {
+          chapter_number: number
+          chapter_revelation_order: number
+          chapter_title_arabic: string
+          chapter_title_english: string
+          chapter_title_transliterated: string
+          global_index: number
+          verse_footnote_english: string | null
+          verse_id: string
+          verse_id_arabic: string
+          verse_index: number
+          verse_number: number
+          verse_subtitle_english: string | null
+          verse_text_arabic: string
+          verse_text_arabic_clean: string
+          verse_text_english: string
+          verse_text_transliterated: string
+        }
+        Insert: {
+          chapter_number: number
+          chapter_revelation_order: number
+          chapter_title_arabic: string
+          chapter_title_english: string
+          chapter_title_transliterated: string
+          global_index?: number
+          verse_footnote_english?: string | null
+          verse_id: string
+          verse_id_arabic: string
+          verse_index: number
+          verse_number: number
+          verse_subtitle_english?: string | null
+          verse_text_arabic: string
+          verse_text_arabic_clean: string
+          verse_text_english: string
+          verse_text_transliterated: string
+        }
+        Update: {
+          chapter_number?: number
+          chapter_revelation_order?: number
+          chapter_title_arabic?: string
+          chapter_title_english?: string
+          chapter_title_transliterated?: string
+          global_index?: number
+          verse_footnote_english?: string | null
+          verse_id?: string
+          verse_id_arabic?: string
+          verse_index?: number
+          verse_number?: number
+          verse_subtitle_english?: string | null
+          verse_text_arabic?: string
+          verse_text_arabic_clean?: string
+          verse_text_english?: string
+          verse_text_transliterated?: string
+        }
+        Relationships: []
+      }
+      "ws-quran-chapters": {
+        Row: {
+          chapter_number: number
+          chapter_revelation_order: string
+          chapter_title_arabic: string
+          chapter_title_english: string
+          chapter_title_transliterated: string
+          chapter_verses: string
+        }
+        Insert: {
+          chapter_number?: number
+          chapter_revelation_order: string
+          chapter_title_arabic: string
+          chapter_title_english: string
+          chapter_title_transliterated: string
+          chapter_verses: string
+        }
+        Update: {
+          chapter_number?: number
+          chapter_revelation_order?: string
+          chapter_title_arabic?: string
+          chapter_title_english?: string
+          chapter_title_transliterated?: string
+          chapter_verses?: string
+        }
+        Relationships: []
+      }
+      "ws-quran-foreign": {
+        Row: {
+          chapter_title_bahasa: string | null
+          chapter_title_french: string | null
+          chapter_title_german: string | null
+          chapter_title_persian: string | null
+          chapter_title_russian: string | null
+          chapter_title_swedish: string | null
+          chapter_title_tamil: string | null
+          chapter_title_turkish: string | null
+          verse_footnote_bahasa: string | null
+          verse_footnote_french: string | null
+          verse_footnote_german: string | null
+          verse_footnote_persian: string | null
+          verse_footnote_russian: string | null
+          verse_footnote_swedish: string | null
+          verse_footnote_tamil: string | null
+          verse_footnote_turkish: string | null
+          verse_id: string
+          verse_subtitle_bahasa: string | null
+          verse_subtitle_french: string | null
+          verse_subtitle_german: string | null
+          verse_subtitle_persian: string | null
+          verse_subtitle_russian: string | null
+          verse_subtitle_swedish: string | null
+          verse_subtitle_tamil: string | null
+          verse_subtitle_turkish: string | null
+          verse_text_bahasa: string | null
+          verse_text_french: string | null
+          verse_text_german: string | null
+          verse_text_persian: string | null
+          verse_text_russian: string | null
+          verse_text_swedish: string | null
+          verse_text_tamil: string | null
+          verse_text_turkish: string | null
+        }
+        Insert: {
+          chapter_title_bahasa?: string | null
+          chapter_title_french?: string | null
+          chapter_title_german?: string | null
+          chapter_title_persian?: string | null
+          chapter_title_russian?: string | null
+          chapter_title_swedish?: string | null
+          chapter_title_tamil?: string | null
+          chapter_title_turkish?: string | null
+          verse_footnote_bahasa?: string | null
+          verse_footnote_french?: string | null
+          verse_footnote_german?: string | null
+          verse_footnote_persian?: string | null
+          verse_footnote_russian?: string | null
+          verse_footnote_swedish?: string | null
+          verse_footnote_tamil?: string | null
+          verse_footnote_turkish?: string | null
+          verse_id: string
+          verse_subtitle_bahasa?: string | null
+          verse_subtitle_french?: string | null
+          verse_subtitle_german?: string | null
+          verse_subtitle_persian?: string | null
+          verse_subtitle_russian?: string | null
+          verse_subtitle_swedish?: string | null
+          verse_subtitle_tamil?: string | null
+          verse_subtitle_turkish?: string | null
+          verse_text_bahasa?: string | null
+          verse_text_french?: string | null
+          verse_text_german?: string | null
+          verse_text_persian?: string | null
+          verse_text_russian?: string | null
+          verse_text_swedish?: string | null
+          verse_text_tamil?: string | null
+          verse_text_turkish?: string | null
+        }
+        Update: {
+          chapter_title_bahasa?: string | null
+          chapter_title_french?: string | null
+          chapter_title_german?: string | null
+          chapter_title_persian?: string | null
+          chapter_title_russian?: string | null
+          chapter_title_swedish?: string | null
+          chapter_title_tamil?: string | null
+          chapter_title_turkish?: string | null
+          verse_footnote_bahasa?: string | null
+          verse_footnote_french?: string | null
+          verse_footnote_german?: string | null
+          verse_footnote_persian?: string | null
+          verse_footnote_russian?: string | null
+          verse_footnote_swedish?: string | null
+          verse_footnote_tamil?: string | null
+          verse_footnote_turkish?: string | null
+          verse_id?: string
+          verse_subtitle_bahasa?: string | null
+          verse_subtitle_french?: string | null
+          verse_subtitle_german?: string | null
+          verse_subtitle_persian?: string | null
+          verse_subtitle_russian?: string | null
+          verse_subtitle_swedish?: string | null
+          verse_subtitle_tamil?: string | null
+          verse_subtitle_turkish?: string | null
+          verse_text_bahasa?: string | null
+          verse_text_french?: string | null
+          verse_text_german?: string | null
+          verse_text_persian?: string | null
+          verse_text_russian?: string | null
+          verse_text_swedish?: string | null
+          verse_text_tamil?: string | null
+          verse_text_turkish?: string | null
+        }
+        Relationships: []
+      }
+      "ws-quran-word-by-word": {
+        Row: {
+          arabic_text: string
+          english_text: string
+          global_index: number
+          root_word: string
+          transliterated_text: string
+          verse_id: string
+          word_index: number
+        }
+        Insert: {
+          arabic_text: string
+          english_text: string
+          global_index?: number
+          root_word: string
+          transliterated_text: string
+          verse_id: string
+          word_index: number
+        }
+        Update: {
+          arabic_text?: string
+          english_text?: string
+          global_index?: number
+          root_word?: string
+          transliterated_text?: string
+          verse_id?: string
+          word_index?: number
+        }
+        Relationships: []
+      }
+      "ws-verse-of-the-day": {
+        Row: {
+          day: number
+          id: string | null
+          month: number
+          verse_id: string
+          year: number
+        }
+        Insert: {
+          day: number
+          id?: string | null
+          month: number
+          verse_id: string
+          year: number
+        }
+        Update: {
+          day?: number
+          id?: string | null
+          month?: number
+          verse_id?: string
+          year?: number
+        }
+        Relationships: []
+      }
     }
     Views: {
       [_ in never]: never
@@ -380,106 +650,106 @@ type DefaultSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
-    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
-    | { schema: keyof Database },
+  | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+  | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+  ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+    Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
-      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+    Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
       Row: infer R
     }
-    ? R
-    : never
+  ? R
+  : never
   : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])
-    ? (DefaultSchema["Tables"] &
-        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
-    : never
+    DefaultSchema["Views"])
+  ? (DefaultSchema["Tables"] &
+    DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+      Row: infer R
+    }
+  ? R
+  : never
+  : never
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema["Tables"]
+  | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+  ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
-    }
-    ? I
-    : never
+    Insert: infer I
+  }
+  ? I
+  : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
-    : never
+  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+    Insert: infer I
+  }
+  ? I
+  : never
+  : never
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
-    | keyof DefaultSchema["Tables"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema["Tables"]
+  | { schema: keyof Database },
   TableName extends DefaultSchemaTableNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+  ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+  : never = never,
 > = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
-    }
-    ? U
-    : never
+    Update: infer U
+  }
+  ? U
+  : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
-    : never
+  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+    Update: infer U
+  }
+  ? U
+  : never
+  : never
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
-    | keyof DefaultSchema["Enums"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema["Enums"]
+  | { schema: keyof Database },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+  ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+  : never = never,
 > = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
   ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-    : never
+  ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+  : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
-    | keyof DefaultSchema["CompositeTypes"]
-    | { schema: keyof Database },
+  | keyof DefaultSchema["CompositeTypes"]
+  | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
     schema: keyof Database
   }
-    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
+  ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+  : never = never,
 > = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
+  ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+  : never
 
 export const Constants = {
   public: {
