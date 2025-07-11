fbfc0d6747607c64da172b36226f535d6285b2ab
Sarim <sarim.xs@gmail.com>
2025-07-10 18:34:23 -0600

    Remove unused permissions in add bot link

--- src/slash-commands/add-bot.ts
+++ src/slash-commands/add-bot.ts
@@ -8,13 +8,14 @@ export default function command(): WSlashCommand {
     description: 'Add the bot to your server',
     execute: async (interaction) => {
       const { clientId } = await Bot.instance.getCredentials();
+      const permissions = 274877925376; // Covers: SEND_MESSAGES, SEND_MESSAGES_IN_THREADS, EMBED_LINKS
       await interaction.reply({
         embeds: [
           new EmbedBuilder()
             .setTitle('Add this bot to your server')
             .setFields({
               name: 'Link',
-              value: `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=274877962240&integration_type=0&scope=bot`,
+              value: `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=${permissions}&integration_type=0&scope=bot`,
             })
             .setThumbnail(
               interaction.client.user?.displayAvatarURL() || null,
