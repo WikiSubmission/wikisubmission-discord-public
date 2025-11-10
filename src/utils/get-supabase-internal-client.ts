7bc07576e8b3e1a160ce8e9539e238ae94c84f7e
Sarim <sarimyx@proton.me>
2025-11-10 04:57:13 -0700

    fix: env key

--- src/utils/get-supabase-internal-client.ts
+++ src/utils/get-supabase-internal-client.ts
@@ -3,13 +3,13 @@ import { Database } from '../types/generated/database.types';
 
 export function getSupabaseInternalClient() {
 
-  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_ROLE_KEY) {
+  if (!process.env.SUPABASE_URL || !process.env.SUPABASE_ANON_KEY) {
     return null;
   }
 
   return createClient<Database, "internal">(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY,
+    process.env.SUPABASE_ANON_KEY,
     {
       db: {
         schema: 'internal'
