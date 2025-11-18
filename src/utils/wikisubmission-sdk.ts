eb4952ad31b0e01b602f313a55536ae4023fd8e4
Sarim <sarimyx@proton.me>
2025-11-18 07:04:54 -0700

    use non cloudflare-proxied db URL (emergency)

--- src/utils/wikisubmission-sdk.ts
+++ src/utils/wikisubmission-sdk.ts
@@ -1,5 +1,6 @@
 import { WikiSubmission } from "wikisubmission-sdk";
 
 export const ws = WikiSubmission.createClient({
+  endpointUrl: process.env.BYPASS_CLOUDFLARE_PROXY_URL || undefined, // (emergency case: use the original supabase URL from env)
   enableLogging: true,
 });
